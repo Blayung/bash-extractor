5c59915176c27549f4fc1b737b83d19dadcb8840
Blayung <gpewojan1@gmail.com>
2022-06-05 21:00:28 +0200

    Added main extract.sh file and modified README.md from default to prettier one.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bash-extractor
+# Bash extractor
 A simple bash script to extract most common archive types.
+**Supported extensions:**
+* zip
+* tar
+* tar.gz (tgz)
+* tar.xz (txz)
---
+++ extract.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Extraction script by Blayung.
+
+if [ -z "$1" ]; then
+    echo "No file name was specified."
+    exit
+elif ! [ -z "$2" ]; then
+    echo "Usage: extract.sh <filename>"
+    exit
+elif ! [ -f "$1" ]; then
+    echo "This file does not exist."
+    exit
+fi
+
+if [ $1 == *.zip ]; then
+    unzip "$1"
+elif [ $1 == *.tar ] || [ $1 == *.tgz ] || [ $1 == *.tar.gz ] || [ $1 == *.tar.xz ] || [ $1 == *.txz ]; then
+    tar -xf "$1"
+else
+    echo "Specified file has unrecognized file extension."
+fi
